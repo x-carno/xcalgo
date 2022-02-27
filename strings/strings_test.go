b33ae4a3b97bd45f5f51b753658a5ab28a9f49b2
zhuyanxi <547110356@qq.com>
2022-02-27 23:01:00 +0800

    Update strings_test.go

--- strings/strings_test.go
+++ strings/strings_test.go
@@ -83,3 +83,32 @@ func TestFirstUniqChar(t *testing.T) {
 		})
 	}
 }
+
+func TestStrStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "y",
+			args: args{
+				haystack: "aaab",
+				needle:   "aab",
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := StrStr(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("StrStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
