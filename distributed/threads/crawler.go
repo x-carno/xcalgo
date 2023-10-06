2d3c0c36eb9afba04f9ed0eedb1a50487ee8f76d
zhuyanxi <547110356@qq.com>
2023-10-06 23:08:24 +0800

    Create crawler.go

---
+++ distributed/threads/crawler.go
@@ -0,0 +1,49 @@
+package threads
+
+import "sync"
+
+//
+// Fetcher
+//
+type Fetcher interface {
+	// Fetch returns a slice of URLs found on the page.
+	Fetch(url string) (urls []string, err error)
+}
+
+//
+// Concurrent crawler with shared state and Mutex
+//
+type fetchState struct {
+	mu      sync.Mutex
+	fetched map[string]bool
+}
+
+func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
+	f.mu.Lock()
+	already := f.fetched[url]
+	f.fetched[url] = true
+	f.mu.Unlock()
+
+	if already {
+		return
+	}
+
+	urls, err := fetcher.Fetch(url)
+	if err != nil {
+		return
+	}
+	var done sync.WaitGroup
+	for _, u := range urls {
+		done.Add(1)
+		u2 := u
+		go func() {
+			defer done.Done()
+			ConcurrentMutex(u2, fetcher, f)
+		}()
+		//go func(u string) {
+		//	defer done.Done()
+		//	ConcurrentMutex(u, fetcher, f)
+		//}(u)
+	}
+	done.Wait()
+}
